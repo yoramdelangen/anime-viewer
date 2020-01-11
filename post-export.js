f190cf1605c328db0cd165e2655677f5cdbdf59e
Yoram de Langen <yoramdelangen@gmail.nl>
2020-01-11 10:01:40 +0100

    Remove netlify stuff

--- post-export.js
+++
@@ -1,7 +0,0 @@
-// copy _redirects file to ./out folder
-const fs = require("fs");
-// destination.txt will be created or overwritten by default.
-fs.copyFile("_redirects", "./out/_redirects", err => {
-  if (err) throw err;
-  console.log("_redirects was copied to ./out/_redirects");
-});
