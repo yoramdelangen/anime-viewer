a4fcefdb43ff74d3bc6790b14cb9dd474f725cbc
Yoram de Langen <yoramdelangen@gmail.nl>
2020-01-14 22:21:56 +0100

    Added checks for homescreen add app button

--- components/OfflineSupport.js
+++ components/OfflineSupport.js
@@ -7,6 +7,17 @@ export default function OfflineSupport() {
         .register("/service-worker.js")
         .then(() => console.log("service worker registered."))
         .catch(err => console.dir(err));
+
+      let deferredPrompt;
+      window.addEventListener("beforeinstallprompt", e => {
+        // Stash the event so it can be triggered later.
+        deferredPrompt = e;
+        showInstallPromotion();
+      });
+
+      window.addEventListener("appinstalled", evt => {
+        console.log("a2hs installed");
+      });
     }
   }, []);
 
